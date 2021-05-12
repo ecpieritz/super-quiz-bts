7f4ffae243e39cc12232dc30b1b49281b3d5a542
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2021-05-12 09:48:24 -0300

    Updated questions

--- src/util/questions.js
+++ src/util/questions.js
@@ -1,56 +1,101 @@
 export default [
     {
-        text: 'Qual é o coletivo de cães?',
+        text: 'What day did BTS debut?',
         answers: [
-            { text: 'MATILHA', correct: true },
-            { text: 'REBANHO', correct: false },
-            { text: 'CARDUME', correct: false },
-            { text: 'MANADA', correct: false }
+            { text: '11/07/2014', correct: false },
+            { text: '11/08/2013', correct: false },
+            { text: '12/06/2013', correct: false },
+            { text: '11/06/2013', correct: true }
         ]
     },
     {
-        text: 'Qual é a maior floresta do planeta?',
+        text: 'Who is the youngest in the group?',
         answers: [
-            { text: 'NEGRA', correct: false },
-            { text: 'DE SHERWOOD', correct: false },
-            { text: 'DA TIJUCA', correct: false },
-            { text: 'AMAZÔNICA', correct: true }
+            { text: 'RM', correct: false },
+            { text: 'V', correct: false },
+            { text: 'JUNGKOOK', correct: true },
+            { text: 'JIMIN', correct: false }
         ]
     },
     {
-        text: 'Quem é o primeiro substituto do Presidente?',
+        text: 'Which of the boys is the look?',
         answers: [
-            { text: 'VEREADOR', correct: false },
-            { text: 'VICE-PRESIDENTE', correct: true },
-            { text: 'SENADOR', correct: false },
-            { text: 'DEPUTADO', correct: false }
+            { text: 'JIMIN', correct: false },
+            { text: 'JIN', correct: true },
+            { text: 'JUNGKOOK', correct: false },
+            { text: 'V', correct: false }
         ]
     },
     {
-        text: 'Segundo o romance, que animal era “Moby Dick”?',
+        text: 'What is the most recent meaning for BTS??',
         answers: [
-            { text: 'TUBARÃO', correct: false },
-            { text: 'GOLFINHO', correct: false },
-            { text: 'POLVO', correct: false },
-            { text: 'BALEIA', correct: true }
+            { text: 'BEHIND THE SCENE', correct: false },
+            { text: 'BEHIND THE SHOW', correct: false },
+            { text: 'BEYOND THE SCENE', correct: true },
+            { text: 'BEYOND THE SHOW', correct: false }
         ]
     },
     {
-        text: 'Que animal é o Pateta?',
+        text: 'What year was Jin born?',
         answers: [
-            { text: 'CAVALO', correct: false },
-            { text: 'BURRO', correct: false },
-            { text: 'CACHORRO', correct: true },
-            { text: 'RAPOSA', correct: false }
+            { text: '1991', correct: false },
+            { text: '1990', correct: false },
+            { text: '1993', correct: false },
+            { text: '1992', correct: true }
         ]
     },
     {
-        text: 'Qual destes personagens não veste uma capa?',
+        text: 'What year was Suga born?',
         answers: [
-            { text: 'SUPER-HOMEM', correct: false },
-            { text: 'BATMAN', correct: false },
-            { text: 'WOLVERINE', correct: true },
-            { text: 'ZORRO', correct: false }
+            { text: '1994', correct: false },
+            { text: '1992', correct: false },
+            { text: '1993', correct: true },
+            { text: '1992', correct: false }
+        ]
+    },
+    {
+        text: 'What year was J-Hope born?',
+        answers: [
+            { text: '1994', correct: true },
+            { text: '1992', correct: false },
+            { text: '1993', correct: false },
+            { text: '1995', correct: false }
+        ]
+    },
+    {
+        text: 'What year was RM born?',
+        answers: [
+            { text: '1992', correct: false },
+            { text: '1993', correct: false },
+            { text: '1995', correct: false },
+            { text: '1994', correct: true }
+        ]
+    },
+    {
+        text: 'What year was Jimin born?',
+        answers: [
+            { text: '1993', correct: false },
+            { text: '1995', correct: true },
+            { text: '1998', correct: false },
+            { text: '1997', correct: false },
+        ]
+    },
+    {
+        text: 'What year was V born?',
+        answers: [
+            { text: '1998', correct: false },
+            { text: '1993', correct: false },
+            { text: '1995', correct: true },
+            { text: '1997', correct: false },
+        ]
+    },
+    {
+        text: 'What year was Jungkook born?',
+        answers: [
+            { text: '1997', correct: true },
+            { text: '1996', correct: false },
+            { text: '1999', correct: false },
+            { text: '1998', correct: false },
         ]
     },
 ]
